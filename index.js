5a311e12e32a725b1db30bb4df6ac02267311e11
Pouya <pouya.eghbali@yandex.com>
2023-05-29 17:20:07 +0200

    Add contract comments, cli options

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ import { readFile, writeFile } from "fs/promises";
 import { program } from "commander";
 
 import { generate } from "./lib/generator.js";
+import { defaultOptions, version } from "./lib/cli.js";
 
 program
   .name("solidq")
   .description(
     "A CLI tool for transforming data structures into decentralized, blockchain-backed databases using Solidity"
   )
-  .version("0.1.0");
+  .version(version);
 
 program
   .command("generate", { isDefault: true })
   .description("generate a database from a schema file")
   .argument("<schema>", "data schema in YAML format")
   .argument("<output>", "output file in Solidity format")
-  .action(async (input, output) => {
+  .option(
+    "-c, --contract <name>",
+    "name of the generated contract",
+    defaultOptions.contractName
+  )
+  .option(
+    "-s, --solidity <name>",
+    "solidity version to use",
+    defaultOptions.solidityVersion
+  )
+  .action(async (input, output, options) => {
     const text = await readFile(input);
     const schema = parse(text.toString());
-    const generated = generate(schema);
+    const generated = generate(schema, options);
     await writeFile(output, generated);
   });
 
